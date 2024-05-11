5607f1662fe20494600cd80cf31e3d1aba27f198
jiyooniboon <jiyooniboon@g.skku.edu>
2024-05-11 16:45:08 -0400

    week8 commit movie2

--- week6/src/components/Movie.js
+++ week6/src/components/Movie.js
@@ -19,10 +19,10 @@ function Movie({id, coverImg, title, summary, genres}) {
 
 Movie.propTypes = {
   id: PropTypes.number.isRequired,
-    coverImg: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default Movie;
--- week6/src/routes/Detail.js
+++ week6/src/routes/Detail.js
@@ -1,5 +1,42 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie"
+
 function Detail() {
-    return <h1>Detail</h1>;
+    const {id} = useParams();
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState();
+
+
+    const getMovie = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        setLoading(false);
+        setMovie(json);
+        //console.log(json);
+    };
+
+    useEffect(() => {
+        getMovie();
+        
+    }, [])
+    
+    
+    
+    return (
+        <div>
+          {loading ? (
+            <h1>Loading...</h1>
+          ) : (
+            <div>
+                <h1>Details</h1>
+              
+            </div>
+            )}
+  
+        </div>
+      );
 }
 
 export default Detail;
