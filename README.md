63d30db139bbf739aa6f53520fe88eb87355e1df
jiyooniboon <jiyooniboon@g.skku.edu>
2024-05-27 14:23:13 +0900

    week10 commit

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 7주차: CRA, EFFECTS (week6 folder) <br>
 8주차: 영화 웹 서비스 만들기 (cont. in week 6 folder) <br>
 9주차: next.js setup, routing <br>
+10주차: next.js data fetching (cont. in week9 folder)<br>
--- week9/app/(movies)/movies/[id]/page.tsx
+++ week9/app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,22 @@
-export default function MovieDetail({
+import { Suspense } from "react";
+import { API_URL } from "../../../(home)/page";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+
+export default async function MovieDetail({
     params: {id},
 }: {
     params: {id: string};
 }) {
-    return <h1>Movie {id}</h1>
+    return (
+        <div>
+            <h3>Movie Details</h3>
+            <Suspense fallback={<h1>Loading movie info</h1>}>
+                <MovieInfo id={id}/>
+            </Suspense>
+            <Suspense fallback={<h1>Loading movie videos</h1>}>
+                <MovieVideos id={id}/>
+            </Suspense> 
+        </div>
+    );
 }
